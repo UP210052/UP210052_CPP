b855996fa5ec6ca9c0389efd20a63875a8f61015
UP210052 <up210052@alumnos.upa.edu.mx>
2022-09-12 10:49:24 -0500

    Programa 01

---
+++ U1/01_Hola.cpp
@@ -0,0 +1,17 @@
+/*
+Date: 12/09/2022
+Author: Héctor Iván Ramírez Jaime.
+Email: up210052@alumnos.upa.edu.mx
+Description: My first program in C++
+*/
+//-------- PREPROCESSOR DIRECTIVES --------
+
+//Input/output library between the screen n' keyboard
+#include <iostream>
+
+//-------- PREPROCESSOR DIRECTIVES --------
+int main(){
+    std::cout <<"Hello world! This is my first program ";
+    //Return 0 means that the program ends succesfully
+    return 0;   
+}
