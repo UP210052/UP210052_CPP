9aef4c97dc88fab2a188886af855833e6a44c32d
UP210052 <up210052@alumnos.upa.edu.mx>
2022-09-22 17:48:03 -0500

    Ejercicio 1

--- U2/01_Renta.cpp
+++ U2/01_Renta.cpp
@@ -7,11 +7,33 @@ Description: Muestra el uso del if else
 #include <iostream>
 using namespace std;
 int main(){
-    int renta;
-    cout << "Ingresa tu renta anual \n";
-    cin >> renta;
-    if (renta<10000){
-
+    float Renta;
+    float Impositivo;
+    float TotalAPagar;
+    cout << "Ingresa tu renta anual: ";
+    cin >> Renta;
+    if (Renta<10000){
+        cout << "El tipo de impositivo es de 5% \n";
+        Impositivo = Renta * .05;
     }
+    if (Renta>=10000 and Renta<20000){
+        cout << "El tipo de impositivo es de 15% \n";
+        Impositivo = Renta * .15;
+    }
+    if (Renta>=20000 and Renta<35000){
+        cout << "El tipo de impositivo es de 20%  \n";
+        Impositivo = Renta * .20;
+    }
+    if (Renta>=35000 and Renta<60000){
+        cout << "El tipo de impositivo es de 30%  \n";
+        Impositivo = Renta * .30;
+    }
+    if (Renta>=60000){
+        cout << "El tipo de impositivo es de 45% \n";
+        Impositivo = Renta * .45;
+    }
+    cout << "El cual equivale a $" << Impositivo << endl;
+    TotalAPagar= Renta + Impositivo;
+    cout << "El total a pagar es de: $" << TotalAPagar << endl;
     return 0;
 }
