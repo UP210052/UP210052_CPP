e616d20ea49b5f73f108f6aa35b57598223cf9c4
UP210052 <up210052@alumnos.upa.edu.mx>
2022-09-22 17:52:22 -0500

    Ejercicio 1

--- U2/01_Renta.cpp
+++ U2/01_Renta.cpp
@@ -4,6 +4,19 @@ Author: Héctor Iván Ramírez Jaime.
 Email: up210052@alumnos.upa.edu.mx
 Description: Muestra el uso del if else
 */
+
+/*
+Los tramos impositivos para la declaración de la renta en un determinado país son los siguientes:
+Renta	Tipo impositivo
+Menos de $10,000	5%
+Entre $10,000 y $20,000	15%
+Entre $20,000 y $35,000	20%
+Entre $35,000 y $60,000	30%
+Más de $60,000	45%
+Escribir un programa que pregunte al usuario su renta anual y muestre por 
+pantalla el tipo impositivo que le corresponde.
+*/
+
 #include <iostream>
 using namespace std;
 int main(){
