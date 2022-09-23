f1c2028fd7da4578a52bdd7bc4466ab83166755c
UP210052 <up210052@alumnos.upa.edu.mx>
2022-09-23 09:58:00 -0500

    Ejercicio 1

--- U2/01_Renta.cpp
+++ U2/01_Renta.cpp
@@ -2,51 +2,62 @@
 Date: 22/09/2022
 Author: Héctor Iván Ramírez Jaime.
 Email: up210052@alumnos.upa.edu.mx
-Description: Muestra el uso del if else
+Description: Rent y Tax
 */
 
 /*
-Los tramos impositivos para la declaración de la renta en un determinado país son los siguientes:
-Renta	Tipo impositivo
+Los tramos Taxs para la declaración de la Rent en un determinado país son los siguientes:
+Rent	Tipo Tax
 Menos de $10,000	5%
 Entre $10,000 y $20,000	15%
 Entre $20,000 y $35,000	20%
 Entre $35,000 y $60,000	30%
 Más de $60,000	45%
-Escribir un programa que pregunte al usuario su renta anual y muestre por 
-pantalla el tipo impositivo que le corresponde.
+Escribir un programa que pregunte al usuario su Rent anual y muestre por 
+pantalla el tipo Tax que le corresponde.
 */
 
+//Libreria  para manejo de entradas y salidas.
 #include <iostream>
+
+//Uso del namespace para evitar el std::
 using namespace std;
+
+//Función principal de tipo entero
 int main(){
-    float Renta;
-    float Impositivo;
-    float TotalAPagar;
-    cout << "Ingresa tu renta anual: ";
-    cin >> Renta;
-    if (Renta<10000){
-        cout << "El tipo de impositivo es de 5% \n";
-        Impositivo = Renta * .05;
+    //Declaración de variables.
+    float Rent;
+    float Tax;
+    
+    //Solicitar la renta.
+    cout << "Enter your annual rent: ";
+    cin >> Rent;
+    
+    //Evaluar la renta (rent) para obtener el porcentaje (tax)
+    if (Rent>=0){
+        if (Rent<10000){
+            Tax = .05;
+        }
+        if (Rent>=10000 and Rent<20000){
+            Tax = .15;
+        } 
+        if (Rent>=20000 and Rent<35000){
+            Tax = .20;
+        }
+        if (Rent>=35000 and Rent<60000){
+            Tax = .30;
+        }
+        if (Rent>=60000){
+            Tax = .45;
+        }
+        
+        //Visualización de datos.
+        cout << "The tax rate is" << (Tax*100) << "% \n";
+        Tax= Rent * Tax;
+        cout << "Which is the equivalent $" << Tax << endl;
     }
-    if (Renta>=10000 and Renta<20000){
-        cout << "El tipo de impositivo es de 15% \n";
-        Impositivo = Renta * .15;
+    else {
+        cout << "The rent you entered is ncorrect. \n";  
     }
-    if (Renta>=20000 and Renta<35000){
-        cout << "El tipo de impositivo es de 20%  \n";
-        Impositivo = Renta * .20;
-    }
-    if (Renta>=35000 and Renta<60000){
-        cout << "El tipo de impositivo es de 30%  \n";
-        Impositivo = Renta * .30;
-    }
-    if (Renta>=60000){
-        cout << "El tipo de impositivo es de 45% \n";
-        Impositivo = Renta * .45;
-    }
-    cout << "El cual equivale a $" << Impositivo << endl;
-    TotalAPagar= Renta + Impositivo;
-    cout << "El total a pagar es de: $" << TotalAPagar << endl;
-    return 0;
+   return 0;
 }
