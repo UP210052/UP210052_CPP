88d389d9c290f72d47977a8a1601d992333e3997
UP210052 <up210052@alumnos.upa.edu.mx>
2022-10-24 10:40:16 -0500

    Ejercicio 1

--- U3/01_Cambio.cpp
+++ U3/01_Cambio.cpp
@@ -31,65 +31,16 @@ void obtenerCambio(int dinero)
 {
     do
     {
-        /*
-        if ((dinero - 500) >= 0)
-        {
-            quinientos++;
-            dinero-=500;
-        }
-        else if ((dinero - 200) >= 0)
-        {
-            docientos++;
-            dinero-=200;
-        }
-        else if ((dinero - 100) >= 0)
-        {
-            cien++;
-            dinero-=100;
-        }
-        else if ((dinero - 50) >= 0)
-        {
-            cincuenta++;
-            dinero-=50;
-        }
-        else if ((dinero - 20) >= 0)
-        {
-            veinte++;
-            dinero-=20;
-        }
-        else if ((dinero - 10) >= 0)
-        {
-            diez++;
-            dinero-=10;
-        }
-        else if ((dinero - 5) >= 0)
-        {
-            cinco++;
-            dinero-=5;
-        }
-        else if ((dinero - 2) >= 0)
-        {
-            dos++;
-            dinero-=2;
-        }
-        else if ((dinero - 1) >= 0)
-        {
-            uno++;
-            dinero-=1;
-        }
-        */
-        /*
-        ((dinero - 500)>=0)?quinientos++, dinero-=500:quinientos;
-        ((dinero - 200)>=0)?docientos++, dinero-=200:docientos;
-        ((dinero - 100)>=0)?cien++, dinero-=100:cien;
-        ((dinero - 50)>=0)?cincuenta++, dinero-=50:cincuenta;
-        ((dinero - 20)>=0)?veinte++, dinero-=20:veinte;
-        ((dinero - 10)>=0)?diez++, dinero-=10:diez;
-        ((dinero - 5)>=0)?cinco++, dinero-=5:cinco;
-        ((dinero - 2)>=0)?dos++, dinero-=2:dos;
-        ((dinero - 1)>=0)?uno++, dinero-=1:uno;
-        */
-        ((dinero - 500)>=0)?quinientos++, dinero-=500:((dinero - 200)>=0)?docientos++, dinero-=200:((dinero - 100)>=0)?cien++, dinero-=100:((dinero - 50)>=0)?cincuenta++, dinero-=50:((dinero - 20)>=0)?veinte++, dinero-=20:((dinero - 10)>=0)?diez++, dinero-=10:((dinero - 5)>=0)?cinco++, dinero-=5:((dinero - 2)>=0)?dos++, dinero-=2:((dinero - 1)>=0)?uno++, dinero-=1:uno;
+        ((dinero - 500) >= 0) ? quinientos++, dinero -= 500 
+            : ((dinero - 200) >= 0) ? docientos++, dinero -= 200 
+                : ((dinero - 100) >= 0) ? cien++, dinero -= 100 
+                    : ((dinero - 50) >= 0) ? cincuenta++, dinero -= 50 
+                        : ((dinero - 20) >= 0) ? veinte++, dinero -= 20 
+                            : ((dinero - 10) >= 0) ? diez++, dinero -= 10 
+                                : ((dinero - 5) >= 0) ? cinco++, dinero -= 5 
+                                    : ((dinero - 2) >= 0) ? dos++, dinero -= 2 
+                                        : ((dinero - 1) >= 0) ? uno++, dinero -= 1 
+                                            : uno;
     } while (dinero > 0);
 }
 void imprimirCambio(int dinero)
