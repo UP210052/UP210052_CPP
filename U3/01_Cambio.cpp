0e11d1f5813ab811b1c102ffad0e7a028c80259f
UP210052 <up210052@alumnos.upa.edu.mx>
2022-10-26 10:02:38 -0500

    Ejercicio 1

--- U3/01_Cambio.cpp
+++ U3/01_Cambio.cpp
@@ -12,47 +12,84 @@ Realizar un programa que de el cambio en monedas y billetes.
 // Use of namespace to avoid std::
 using namespace std;
 
-void obtenerCambio(int);
-void obtenerMonedas(int);
-void imprimirCambio(int);
+void obtenerCambio(int, string);
+void imprimirCambio(string);
 
+//Declare the global variables.
 int quinientos = 0, docientos = 0, cien = 0, cincuenta = 0, veinte = 0, diez = 0, cinco = 0, dos = 0, uno = 0;
 
 int main()
 {
+    //Declare the variables.
     int dinero;
+    string tipo;
+
+    //Ask for the amount of money and type change
     cout << "Enter the money: ";
     cin >> dinero;
-    obtenerCambio(dinero);
-    imprimirCambio(dinero);
+    cout << "Enter type change whit coin or total:";
+    cin >> tipo;
+
+    //Call the functions 
+    obtenerCambio(dinero, tipo);
+    imprimirCambio(tipo);
     return 0;
 }
-void obtenerCambio(int dinero)
+
+//function to get the change
+void obtenerCambio(int dinero, string tipo)
 {
+    // Do while subtracts the bills or coins until reaching 0
     do
     {
-        ((dinero - 500) >= 0) ? quinientos++, dinero -= 500 
-            : ((dinero - 200) >= 0) ? docientos++, dinero -= 200 
-                : ((dinero - 100) >= 0) ? cien++, dinero -= 100 
-                    : ((dinero - 50) >= 0) ? cincuenta++, dinero -= 50 
-                        : ((dinero - 20) >= 0) ? veinte++, dinero -= 20 
-                            : ((dinero - 10) >= 0) ? diez++, dinero -= 10 
-                                : ((dinero - 5) >= 0) ? cinco++, dinero -= 5 
-                                    : ((dinero - 2) >= 0) ? dos++, dinero -= 2 
-                                        : ((dinero - 1) >= 0) ? uno++, dinero -= 1 
-                                            : uno;
+        
+        if (tipo=="total" || tipo == "TOTAL" || tipo == "Total") //Using bills and coins
+        {
+            // If ternary to subtract the coins and bills from money
+            ((dinero - 500) >= 0) ? quinientos++, dinero -= 500 
+                : ((dinero - 200) >= 0) ? docientos++, dinero -= 200 
+                    : ((dinero - 100) >= 0) ? cien++, dinero -= 100 
+                        : ((dinero - 50) >= 0) ? cincuenta++, dinero -= 50 
+                            : ((dinero - 20) >= 0) ? veinte++, dinero -= 20 
+                                : ((dinero - 10) >= 0) ? diez++, dinero -= 10 
+                                    : ((dinero - 5) >= 0) ? cinco++, dinero -= 5 
+                                        : ((dinero - 2) >= 0) ? dos++, dinero -= 2 
+                                            : ((dinero - 1) >= 0) ? uno++, dinero -= 1 
+                                                : uno;
+        }else if (tipo == "coin" || tipo == "COIN" || tipo == "Coin") //Using coins
+        {
+            //If ternary to subtract the coins from money
+            ((dinero - 10) >= 0) ? diez++, dinero -= 10 
+                : ((dinero - 5) >= 0) ? cinco++, dinero -= 5 
+                    : ((dinero - 2) >= 0) ? dos++, dinero -= 2 
+                        : ((dinero - 1) >= 0) ? uno++, dinero -= 1 
+                            : uno;
+        }
+        
     } while (dinero > 0);
 }
-void imprimirCambio(int dinero)
+
+//Function to print the change
+void imprimirCambio(string tipo)
 {
-    cout << "El cambio de $" << dinero << " es: \n";
-    cout << "Billete de $500: " << quinientos << endl;
-    cout << "Billete de $200: " << docientos << endl;
-    cout << "Billete de $100: " << cien << endl;
-    cout << "Billete de $50: " << cincuenta << endl;
-    cout << "Billete de $20: " << veinte << endl;
-    cout << "Moneda de $10: " << diez << endl;
-    cout << "Moneda de $5: " << cinco << endl;
-    cout << "Moneda de $2: " << dos << endl;
-    cout << "Moneda de $1: " << uno << endl;
+    cout << "The change is : \n";
+    if (tipo == "total" || tipo == "TOTAL" || tipo == "Total")
+    {
+        cout << "Bill of $500: " << quinientos << endl;
+        cout << "Bill of $200: " << docientos << endl;
+        cout << "Bill of $100: " << cien << endl;
+        cout << "Bill of $50: " << cincuenta << endl;
+        cout << "Bill of $20: " << veinte << endl;
+        cout << "Coin of $10: " << diez << endl;
+        cout << "Coin of $5: " << cinco << endl;
+        cout << "Coin of $2: " << dos << endl;
+        cout << "Coin of $1: " << uno << endl;
+    }else if (tipo == "coin" || tipo == "COIN" || tipo == "Coin") 
+    {
+        cout << "Coin of $10: " << diez << endl;
+        cout << "Coin of $5: " << cinco << endl;
+        cout << "Coin of $2: " << dos << endl;
+        cout << "Coin of $1: " << uno << endl;
+    }
+    
 }
