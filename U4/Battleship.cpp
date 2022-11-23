7bd3c94ad2789cbd7a0b2175bf661374228f458c
UP210878 <UP210878@alumnos.edu.upa.mx>
2022-11-23 10:35:33 -0600

    Battleship

--- U4/Battleship.cpp
+++ U4/Battleship.cpp
@@ -2,21 +2,48 @@
 
 using namespace std;
 
-
+string AreaJuegoP1[10][10]={};
+string AreaJuegoP2[10][10]={};
+const string P1 = "Player 1";
+const string P2 = "Player 2";
 
 void tableronaval();
 
+void logicbattleship();
+void buildships();
+void putships(string, int, int);
+int selectplay(char, int);
+bool checkplay(int);
+void shootcannons(string, int);
+void fuckship(string, int);
+bool Winner();
 
-int main(){
+
+
+
+int main()
+{
 
     return 0;
 }
 
-void tableronaval(){
+void tableronaval()
+{
 
-    
 }
-*/fgsdf
-*/ uoj
-jbkj 
-jhgkj
+
+void logicanaval()
+{
+
+}
+
+void buildships(){
+    char Carrier[5]={'O','O','O','O','O'};
+    char Battleship[4]={'O','O','O','O'};
+    char Cruiser[3];
+    char Destroyer[2];
+    char Destroyer2[2];
+    char Submarine[1];
+    char Submarine2[1];
+    char Submarine3[1];
+}
