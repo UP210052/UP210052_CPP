210af61bcc5f19e42b60b3cf6169e489c7d526b3
UP210052 <up210052@alumnos.upa.edu.mx>
2022-11-24 10:08:37 -0600

    Estructura Battleship

--- U4/Battleship.cpp
+++ U4/Battleship.cpp
@@ -2,18 +2,19 @@
 
 using namespace std;
 
-string AreaJuegoP1[10][10]={};
-string AreaJuegoP2[10][10]={};
+char AreaJuegoP1[10][10]={{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}};
+char AreaJuegoP2[10][10]={{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}};
 const string P1 = "Player 1";
 const string P2 = "Player 2";
+int col, row;
 
-void tableronaval();
+void makeboard();
 
 void logicbattleship();
 void buildships();
 void putships(string, int, int);
 int selectplay(char, int);
-bool checkplay(int);
+bool checkplay(int, int, int);
 void shootcannons(string, int);
 void fuckship(string, int);
 bool Winner();
@@ -23,7 +24,16 @@ bool Winner();
 
 int main()
 {
-
+    int col, row, player;
+    bool placeOccupied = true;
+    //makeboard();
+    //preguntartipodebarco();
+    //preguntarcordenada();
+    //selectplay();
+    placeOccupied = checkplay (1, 2, 2);
+    cout << placeOccupied;
+    //putships();
+    //repetir las hasta poner todos los barcos por cada jugador
     return 0;
 }
 
@@ -37,13 +47,34 @@ void logicanaval()
 
 }
 
-void buildships(){
-    char Carrier[5]={'O','O','O','O','O'};
-    char Battleship[4]={'O','O','O','O'};
-    char Cruiser[3];
-    char Destroyer[2];
-    char Destroyer2[2];
-    char Submarine[1];
-    char Submarine2[1];
-    char Submarine3[1];
+bool checkplay (int player, int move, int move2){
+    for (int row = 0; row < 10; row++)
+    {
+        for (int col = 0; col < 10; col++)
+        {
+            if (player == 1){
+                if (AreaJuegoP1[row][col] == 'O')
+                {
+                    return true;
+                }
+            }else if (player == 2){
+                if (AreaJuegoP2[row][col] =='O')
+                {
+                    return true;
+                }
+            }
+        }
+        
+    }
+    return false;
+}
+
+int menuBarcos(){
+    /*
+    1 Submarine [1] (3)
+    2 Destroyer [2] (2)
+    3 Cruiser  [3]
+    4 Battleship 
+    5 Carrier
+    */
 }
